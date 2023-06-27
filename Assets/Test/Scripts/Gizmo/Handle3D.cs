75dd57adc7d08dbd59fd102c8622ca6a5e7b8451
SeanNi1221 <sean.ni.21@outlook.com>
2023-06-27 14:01:07 +0800

    Finish handle 3D shader

--- Assets/Test/Scripts/Gizmo/Handle3D.cs
+++ Assets/Test/Scripts/Gizmo/Handle3D.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Handle3D : Gizmo {
-  [SerializeField] protected Color _normalColor = new Color(0.490196f, 1f, 0.3137253f, 0.3f);
-  [SerializeField] protected Color _hoveredColor = new Color(0.490196f, 1f, 0.3137253f, 0.5f);
-  [SerializeField] protected Color _draggedColor = new Color(0.490196f, 1f, 0.3137253f, 1f);
+  protected static readonly Dictionary<GizmoAnchor, Color> _anchorToColor =
+      new Dictionary<GizmoAnchor, Color> {
+    { GizmoAnchor.X, new Color(1, 0.1f, 0.2f) },
+    { GizmoAnchor.Y, new Color(0.2f, 1, 0.1f) },
+    { GizmoAnchor.Z, new Color(0, 0.4f, 1) },
+  };
+
+  protected const float _normalAlpha = 0.3f;
+  protected const float _hoveredAlpha = 0.5f;
+  protected const float _draggedAlpha = 1f;
+  private const string _baseColorRef = "_BaseColor";
   [SerializeField] protected Renderer _renderer;
+  private MaterialPropertyBlock _propertyBlock;
+  private Color _normalColor;
+  private Color _hoveredColor;
+  private Color _draggedColor;
+
 
   protected virtual void OnEnable() {
     transform.position = GameManager.Selected.transform.position;
@@ -15,20 +28,23 @@ public class Handle3D : Gizmo {
   protected virtual void OnMouseEnter() {
     if (Dragged != this) {
       Hovered = this;
-      _renderer.material.color = _hoveredColor;
+      _propertyBlock.SetColor(_baseColorRef, _hoveredColor);
+      _renderer.SetPropertyBlock(_propertyBlock, 0);
     }
   }
 
   protected virtual void OnMouseExit() {
     if (Dragged != this && Hovered == this) {
       Hovered = null;
-      _renderer.material.color = _normalColor;
+      _propertyBlock.SetColor(_baseColorRef, _normalColor);
+      _renderer.SetPropertyBlock(_propertyBlock, 0);
     }
   }
 
   protected virtual void OnMouseDown() {
     Dragged = this;
-    _renderer.material.color = _draggedColor;
+    _propertyBlock.SetColor(_baseColorRef, _draggedColor);
+    _renderer.SetPropertyBlock(_propertyBlock, 0);
     HideGroup(GizmoGroup.Frame);
     HideGroup(GizmoGroup.MoveHandle);
     HideGroup(GizmoGroup.RotateHandle);
@@ -37,7 +53,8 @@ public class Handle3D : Gizmo {
   protected virtual void OnMouseUp() {
     if (Dragged == this) {
       Dragged = null;
-      _renderer.material.color = _normalColor;
+      _propertyBlock.SetColor(_baseColorRef, _normalColor);
+      _renderer.SetPropertyBlock(_propertyBlock, 0);
       ShowGroup(GizmoGroup.Frame);
       StartCoroutine(FlexibleHandle.AdaptVisibilitiesCoroutine());
     }
@@ -46,7 +63,23 @@ public class Handle3D : Gizmo {
   protected override void Awake() {
     base.Awake();
     Hide();
-    _renderer.material.color = _normalColor;
+    _normalColor = new Color(_anchorToColor[Anchor].r,
+                             _anchorToColor[Anchor].g,
+                             _anchorToColor[Anchor].b,
+                             _normalAlpha);
+    _hoveredColor = new Color(_anchorToColor[Anchor].r,
+                              _anchorToColor[Anchor].g,
+                              _anchorToColor[Anchor].b,
+                              _hoveredAlpha);
+    _draggedColor = new Color(_anchorToColor[Anchor].r,
+                              _anchorToColor[Anchor].g,
+                              _anchorToColor[Anchor].b,
+                              _draggedAlpha);
+    _propertyBlock = new MaterialPropertyBlock();
+    _renderer.GetPropertyBlock(_propertyBlock, 0);
+    _propertyBlock.SetColor(_baseColorRef, _normalColor);
+
+    _renderer.SetPropertyBlock(_propertyBlock, 0);
   }
 
   protected override void OnValidate() {
